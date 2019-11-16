2164cb911fe6424c2037c45a796607c4de52854c
juheon97 <son16007@byui.edu>
2019-11-15 20:02:01 -0700

    assignment 9

---
+++ calculateRate.js
@@ -0,0 +1,124 @@
+function letterConvert(weight) {
+    var rate
+    if (weight >= 3.5 && weight < 4) {
+        rate = 1.45
+    }
+    else if (weight >= 4 && weight < 5) {
+        rate = 1.60
+    }
+    else if (weight >= 5 && weight < 6) {
+        rate = 1.75
+    }
+    else if (weight >= 4 && weight < 5) {
+        rate = 1.90
+    }
+    else if (weight >= 6 && weight < 7) {
+        rate = 2.05
+    }
+    else if (weight >= 7 && weight < 8) {
+        rate = 2.20
+    }
+    else if (weight >= 8 && weight < 9) {
+        rate = 2.35
+    }
+    else if (weight >= 9 && weight < 10) {
+        rate = 2.50
+    }
+    else if (weight >= 10 && weight < 11) {
+        rate = 2.35
+    }
+    else if (weight >= 11 && weight < 12) {
+        rate = 2.65
+    }
+    else if (weight >= 12  && weight <= 13) {
+        rate = 2.80
+    }
+    return rate;
+}
+
+function getPackageCost(weight, type) {
+    var rate;
+    if (type == "First-Class Package Service" && weight <= 4) {
+        rate = 3.66
+    }
+    else if (type == "First-Class Package Service" && weight <= 8 && weight > 4) {
+        rate = 4.36
+    }
+    else if (type == "First-Class Package Service" && weight <= 12 && weight > 8) {
+        rate = 5.19
+    }
+    else if (type == "First-Class Package Service" && weight <= 13 && weight > 12) {
+        rate = 5.71
+    }
+    return rate
+}
+
+function getMeteredCost(weight, type) {
+    var rate;
+    if (type == "Letters (Metered)" && weight <= 1) {
+        rate = 0.50
+    }
+    else if(type == "Letters (Metered)" && weight <= 2 && weight > 1) {
+        rate = 0.65
+    }
+    else if(type == "Letters (Metered)" && weight <= 3 && weight > 2) {   
+        rate  = 0.8 
+    }
+    else if(type == "Letters (Metered)" && weight <= 3.5 && weight > 3) {
+        rate = 0.95
+    }
+    else if(type =="Letters (Metered)" && weight > 3.5) {
+        rate = letterConvert(weight)
+    }
+    return rate;
+}
+
+function getStampedCost(weight, type) {
+    var rate;
+    if (type == "Letters (Stamped)" && weight <= 1) {
+        rate = 0.55
+    }
+    else if(type == "Letters (Stamped)" && weight <= 2 && weight > 1) {
+        rate = 0.70
+    }
+    else if(type == "Letters (Stamped)" && weight <= 3 && weight > 2) {   
+        rate  = 0.85   
+    }
+    else if(type == "Letters (Stamped)" && weight <= 3.5 && weight > 3) {
+        rate = 1.00
+    }
+    else if(type =="Letters (Stamped)" && weight > 3.5) {
+        rate = letterConvert(weight)
+    }
+    return rate;
+}
+
+function convertRate(req, res) {
+    var type = req.query.postal_option;
+    var weight = req.query.weight;
+    var rate;
+    var condition;
+    if (type == "Letters (Stamped)") {
+        rate = getStampedCost(weight,type);
+    }
+    else if(type == "Letters (Metered)") {
+        rate = getMeteredCost(weight, type);
+    }
+    else if (type == "Large Envelopes (Flats)"){
+        rate = getFlatsCost(weight, type);
+    }
+    else if (type == "First-Class Package Service"){
+        rate = getPackageCost(weight, type);
+    } 
+
+    if ((type == "Letters (Stamped)" || type == "Letters (Metered)") && weight > 3.5) {
+        condition = "For weights over 3.5 ounces, it converts to Large Envelopes prices."
+    }
+    else {
+        condition = "";
+    }
+    var stuff = {type: type, rate: rate, weight: weight, condition: condition};
+    res.render('result', stuff);
+}
+
+module.exports = {convertRate: convertRate};
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const calculateRate = require("./calculateRate.js")
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.set('port', process.env.PORT || 5000)
+   .use(express.static(__dirname + '/public'))
+   .set('views', __dirname + '/views')
+   .set('view engine', 'ejs')
+   .get('/', function(req, res){
+      res.sendFile('form.html', { root: __dirname + "/public"});
+   })
+   .get('/getRate', calculateRate.convertRate)
+
+   .listen(app.get('port'), function() {
+      console.log('Listening on port: ' + app.get('port'));
+   })
+
+
+
+
